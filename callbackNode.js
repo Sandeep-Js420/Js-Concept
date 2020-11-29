4829f78dab657325af459c86adef4d5209664081
Stonner420 <jenasandeep76@gmail.com>
2020-11-29 09:53:26 +0530

    Callback Abstraction

--- callbackNode.js
+++ callbackNode.js
@@ -1,7 +1,23 @@
+/**
+ * @author:Sandeep Kumar jena
+ * topic:JavaScript Callback
+ * When a function pass into a another function as a argument  then we can call that passed function as a callback.
+ * Below Example shows the some callback example .
+ * And some callback abstraction. 
+ */
 setTimeout(()=>{
  console.log(' callback function !! Example')
 },2000)
 
+let carArray=['KIA Sonet','Honda WRV','Audi R8','alto lsi'];
+ let shortName=carArray.map((car)=>{
+  return  car.substring(4);
+
+})
+
+console.log(shortName);
+
+//Callback abstraction
 const geocode=function (address,callback){
     setTimeout(()=>{
       
@@ -23,7 +39,7 @@ geocode('bhubaneswar',(data)=>{
        console.log(data)
         })
 
-
+//Another Example of Callback abstraction
 const sum = function(a,b,callback){
     setTimeout(()=>{
          
